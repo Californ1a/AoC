2b0daca50119f32832af16bf4d95c5d1d46314c0
Californ1a <rk_california@yahoo.com>
2020-12-03 17:56:46 -0500

    day 3 part 2

---
+++ 3/part1.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+async function readInput(filename) {
+	const input = await fs.promises.readFile(filename, "utf-8");
+	return input.replace(/\r\n/g, "\n").trim().split("\n");
+}
+
+function puzzle(input, across, down = 1) {
+	let a = across;
+	let treeCount = 0;
+	for (let i = down; i < input.length; i += down) {
+		const line = input[i];
+		if (line[a % line.length] === "#") {
+			treeCount += 1;
+		}
+		a += 3;
+	}
+	return treeCount;
+}
+
+readInput("input.txt").then((input) => {
+	const answer = puzzle(input, 3);
+	console.log(answer);
+});
---
+++ 3/part2.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+
+async function readInput(filename) {
+	const input = await fs.promises.readFile(filename, "utf-8");
+	return input.replace(/\r\n/g, "\n").trim().split("\n");
+}
+
+function puzzle(input, across, down = 1) {
+	let a = across;
+	let treeCount = 0;
+	for (let i = down; i < input.length; i += down) {
+		const line = input[i];
+		// console.log({
+		// 	across: a,
+		// 	down,
+		// 	mod: a % line.length,
+		// 	line,
+		// 	char: line[a % line.length],
+		// });
+		if (line[a % line.length] === "#") {
+			treeCount += 1;
+		}
+		a += across;
+	}
+	return treeCount;
+}
+
+const func = (i) => (a, d) => puzzle(i, a, d);
+
+readInput("input.txt").then((input) => {
+	const p = func(input);
+	const answers = [p(1, 1), p(3, 1), p(5, 1), p(7, 1), p(1, 2)];
+	console.log(answers);
+	const answer = answers.reduce((v, a) => v * a, 1);
+	console.log(answer);
+});
