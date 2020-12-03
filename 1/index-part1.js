1f72ee177394a1a59505d731361259a2debb1864
Californ1a <rk_california@yahoo.com>
2020-12-03 16:08:35 -0500

    day 1

---
+++ 1/index-part1.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+
+async function readInput(filename) {
+	const input = await fs.promises.readFile(filename, "utf-8");
+	return input.split("\n");
+}
+
+function puzzle(input) {
+	const indicies = [];
+	let num1;
+	let num2;
+	for (let i = 0; i < input.length; i += 1) {
+		num1 = parseInt(input[i], 10);
+		for (let j = i + 1; j < input.length - 1; j += 1) {
+			num2 = parseInt(input[j], 10);
+			console.log(`[CAL] i:${i} num1:${num1}, j:${j} num2:${num2}`);
+			if (num1 + num2 === 2020) {
+				indicies.push({
+					i,
+					num1,
+					j,
+					num2,
+				});
+			}
+		}
+	}
+	return indicies;
+}
+
+readInput("input-part1.txt").then((input) => {
+	const answers = puzzle(input);
+	answers.forEach((answer, i) => {
+		const mult = answer.num1 * answer.num2;
+		console.log(answer);
+		console.log(`${i}: ${mult}`);
+	});
+});
---
+++ 1/index-part2.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+
+async function readInput(filename) {
+	const input = await fs.promises.readFile(filename, "utf-8");
+	return input.split("\n");
+}
+
+function puzzle(input) {
+	const indicies = [];
+	let num1;
+	let num2;
+	let num3;
+	for (let i = 0; i < input.length; i += 1) {
+		num1 = parseInt(input[i], 10);
+		for (let j = i + 1; j < input.length - 1; j += 1) {
+			num2 = parseInt(input[j], 10);
+			for (let k = j + 1; k < input.length - 1; k += 1) {
+				num3 = parseInt(input[k], 10);
+				console.log(`[CAL] i:${i} num1:${num1}, j:${j} num2:${num2}, k:${k} num3:${num3}`);
+				if (num1 + num2 + num3 === 2020) {
+					indicies.push({
+						i,
+						num1,
+						j,
+						num2,
+						k,
+						num3,
+					});
+				}
+			}
+		}
+	}
+	return indicies;
+}
+
+readInput("input-part2.txt").then((input) => {
+	const answers = puzzle(input);
+	answers.forEach((answer, i) => {
+		const mult = answer.num1 * answer.num2 * answer.num3;
+		console.log(answer);
+		console.log(`${i}: ${mult}`);
+	});
+});
