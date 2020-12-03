185038963809b6e167335bddcc7984ea69337aed
Californ1a <rk_california@yahoo.com>
2020-12-03 16:57:06 -0500

    day 2 part 1

---
+++ 2/index.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+
+async function readInput(filename) {
+	const input = await fs.promises.readFile(filename, "utf-8");
+	return input.replace(/\r\n/g, "\n").trim().split("\n");
+}
+
+function puzzle(input) {
+	let sum = 0;
+	input.forEach((line) => {
+		const reg = /^(\d+)-(\d+) (\w): (.+)$/gi;
+		const matches = reg.exec(line);
+		const minimum = matches[1];
+		const maximum = matches[2];
+		const letter = matches[3];
+		const password = matches[4];
+		const regex = new RegExp(letter, "gi");
+		const letters = password.match(regex);
+		if (letters) {
+			const passTest = letters.length >= minimum && letters.length <= maximum;
+			if (passTest) {
+				sum += 1;
+			}
+		}
+	});
+	return sum;
+}
+
+readInput("input.txt").then((input) => {
+	const answer = puzzle(input);
+	console.log(answer);
+});
