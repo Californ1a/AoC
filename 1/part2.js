c04ea606b2e0670d1e08f9f98fe1512b1be5df19
Californ1a <rk_california@yahoo.com>
2020-12-03 17:15:24 -0500

    day 2 part 2

--- 1/part2.js
+++ 1/part2.js
@@ -9,28 +9,34 @@ function puzzle(input) {
 	const indicies = [];
 	let num1;
 	let num2;
+	let num3;
 	for (let i = 0; i < input.length; i += 1) {
 		num1 = parseInt(input[i], 10);
 		for (let j = i + 1; j < input.length - 1; j += 1) {
 			num2 = parseInt(input[j], 10);
-			console.log(`[CAL] i:${i} num1:${num1}, j:${j} num2:${num2}`);
-			if (num1 + num2 === 2020) {
-				indicies.push({
-					i,
-					num1,
-					j,
-					num2,
-				});
+			for (let k = j + 1; k < input.length - 1; k += 1) {
+				num3 = parseInt(input[k], 10);
+				console.log(`[CAL] i:${i} num1:${num1}, j:${j} num2:${num2}, k:${k} num3:${num3}`);
+				if (num1 + num2 + num3 === 2020) {
+					indicies.push({
+						i,
+						num1,
+						j,
+						num2,
+						k,
+						num3,
+					});
+				}
 			}
 		}
 	}
 	return indicies;
 }
 
-readInput("input-part1.txt").then((input) => {
+readInput("input.txt").then((input) => {
 	const answers = puzzle(input);
 	answers.forEach((answer, i) => {
-		const mult = answer.num1 * answer.num2;
+		const mult = answer.num1 * answer.num2 * answer.num3;
 		console.log(answer);
 		console.log(`${i}: ${mult}`);
 	});
---
+++ 2/part2.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+
+async function readInput(filename) {
+	const input = await fs.promises.readFile(filename, "utf-8");
+	return input.replace(/\r\n/g, "\n").trim().split("\n");
+}
+
+function puzzle(input) {
+	let sum = 0;
+	input.forEach((line) => {
+		const reg = /^(\d+)-(\d+) (\w): (.+)$/gi;
+		const matches = reg.exec(line);
+		const index1 = parseInt(matches[1], 10) - 1;
+		const index2 = parseInt(matches[2], 10) - 1;
+		const letter = matches[3];
+		const password = matches[4];
+		// console.log({
+		// 	password,
+		// 	letter,
+		// 	index1: password[index1],
+		// 	index2: password[index2],
+		// });
+		if (password[index1] === letter) {
+			if (password[index2] !== letter) {
+				sum += 1;
+			}
+		} else if (password[index2] === letter) {
+			if (password[index1] !== letter) {
+				sum += 1;
+			}
+		}
+	});
+	return sum;
+}
+
+readInput("input.txt").then((input) => {
+	const answer = puzzle(input);
+	console.log(answer);
+});
